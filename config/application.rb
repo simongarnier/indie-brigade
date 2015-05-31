e6e3cb1ec73c39484eeee65f320586e7a845e8fa
Simon Garnier <simongarnierg@gmail.com>
2015-05-30 21:04:58 -0300

    enable bootstrap

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Indiebrigade
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,5 +23,7 @@ module Indiebrigade
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    puts config.sass.load_paths
+
   end
 end
