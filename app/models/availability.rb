7aae0f4acb5796c7d415cd9eb5258dd703845f57
Simon Garnier <simongarnierg@gmail.com>
2015-06-19 21:28:32 -0300

    fix relation between availability and related tables

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,2 +1,10 @@
 class Availability < ActiveRecord::Base
+    belongs_to :availability_per_week
+    belongs_to :availability_duration
+
+    attr_reader :name
+
+    def name
+        "#{self.availability_per_week.name} :: #{self.availability_duration.name}"
+    end
 end
