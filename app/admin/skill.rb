e33d23baa1c9a10f17969cc3b156d71e68277552
Simon Garnier <simongarnierg@gmail.com>
2015-07-18 13:20:11 -0300

    bunch of stuff including skills edition in admin

--- app/admin/skill.rb
+++ app/admin/skill.rb
@@ -7,12 +7,23 @@ ActiveAdmin.register Skill do
   # permit_params :list, :of, :attributes, :on, :model
   #
   # or
-  #
-  permit_params do
-    permitted = [:short, :name, :role_id]
+  show do
+    attributes_table do
+      row :short_name
+      row :long_name
+      row :role do |skill|
+        skill.role.code
+      end
+    end
   end
 
   form do |f|
-      f.input :role_id
+    f.inputs 'skill' do
+      f.input :short_name
+      f.input :long_name
+      f.input :role, as: :select, collection: Role.all.collect {|role| [role.code, role.id] }
+    end
+    f.actions
   end
+  permit_params :short_name, :long_name, :role_id
 end
