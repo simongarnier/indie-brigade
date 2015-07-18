cb77caa8747546caa8ac23fe329d9ff7a06fefb9
Simon Garnier <simongarnierg@gmail.com>
2015-07-18 14:00:48 -0300

    cleaner edit/index skills page

--- app/admin/skill.rb
+++ app/admin/skill.rb
@@ -1,12 +1,10 @@
 ActiveAdmin.register Skill do
+  controller do
+    def scoped_collection
+      super.includes :role # prevents N+1 queries to your database
+    end
+  end
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
   show do
     attributes_table do
       row :short_name
@@ -19,11 +17,22 @@ ActiveAdmin.register Skill do
 
   form do |f|
     f.inputs 'skill' do
-      f.input :short_name
-      f.input :long_name
+      f.input :short_name, as: :string
+      f.input :long_name, as: :string
       f.input :role, as: :select, collection: Role.all.collect {|role| [role.code, role.id] }
     end
     f.actions
   end
+
+  index do
+    selectable_column
+    column :short_name
+    column :long_name
+    column :role, sortable: 'roles.code' do |skill|
+      link_to skill.role.code, admin_role_path(skill.role)
+    end
+    actions
+  end
+
   permit_params :short_name, :long_name, :role_id
 end
