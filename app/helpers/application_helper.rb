adc2924333805305d12ad946cf4e462f070fdf90
Simon Garnier <simongarnierg@gmail.com>
2015-07-18 19:23:14 -0300

    nav bar desktop and mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_user_dev
+    return current_user.try(:dev)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   resources :devs do
-    resources :skills, only: [:index]
-    resources :dev_skills, only: [:create]
-    resources :availabilities, :softwares
+    resources :skills, :availabilities, :softwares, only: [:index]
+    resources :dev_skills, :dev_conditions, :dev_availabilities, only: [:create]
   end
 
 
