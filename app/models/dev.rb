e097959c7c3555ce26ebc943e4fa8cec6df4bb26
Simon Garnier <simongarnierg@gmail.com>
2015-09-22 15:59:00 -0300

    when a dev is deleted, delete dependant object

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -2,18 +2,16 @@ class Dev < ActiveRecord::Base
   belongs_to :role
   belongs_to :availability
   belongs_to :user
-
-  has_many :dev_conditions
-  has_many :conditions, through: :dev_conditions
-
-  has_many :dev_major_skills
-  has_many :major_skills, :through => :dev_major_skills, :source => :skill
-
-  has_many :dev_minor_skills
-  has_many :minor_skills, :through => :dev_minor_skills, :source => :skill
-
   belongs_to :main_skill, class_name: 'Skill', foreign_key: 'main_skill_id'
 
+  has_many :dev_conditions, dependent: :delete_all
+  has_many :conditions, through: :dev_conditions
+
+  has_many :dev_major_skills , dependent: :delete_all
+  has_many :major_skills, through: :dev_major_skills, source: :skill
+
+  has_many :dev_minor_skills, dependent: :delete_all
+  has_many :minor_skills, through: :dev_minor_skills, source: :skill
 
   attr_reader :skills
 
