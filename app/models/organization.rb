33ccc0ff72857fa5fa217f80ea61f402b33a3b08
Simon Garnier <simongarnierg@gmail.com>
2015-10-21 20:59:49 -0300

    using clearance token in organiztion for consistency

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,11 @@
+require 'clearance/token'
+
 class Organization < ActiveRecord::Base
   belongs_to :user
 
   before_create do
     self.member_code = loop do
-      random_token = SecureRandom.hex(10)
+      random_token =  Clearance::Token.new
       break random_token unless self.class.exists?(member_code: random_token)
     end
   end
