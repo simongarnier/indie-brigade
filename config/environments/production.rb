1e1b522b198a2091bc7b3df0b0147641aa717212
Simon Garnier <simongarnierg@gmail.com>
2015-10-22 20:35:06 -0300

    fixed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.identity_cache_store = :mem_cache_store, Dalli::Client.new('localhost:11211')
-
-  config.default_url_options[:host] = 'indiebrigade.com'
 end
+
+Rails.application.routes.default_url_options[:host] = 'indiebrigade.com'
