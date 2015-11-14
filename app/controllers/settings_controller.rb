f0c4f4748bd049713730f126949388e571729c42
Simon Garnier <simongarnierg@gmail.com>
2015-11-14 15:57:24 -0500

    skills are now grouped by role

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,7 +4,7 @@ class SettingsController < ApplicationController
   before_action :ensure_dev_owned_by_current_user, only: [:edit]
 
   def edit_skills
-    @skills = Skill.all
+    @skills = Skill.all.group_by(&:role)
     @major_skills = current_user.dev.major_skills
     @minor_skills = current_user.dev.minor_skills
   end
