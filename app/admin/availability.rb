e158cb326e5db1d9bab16998d6cb8390985f3ad9
Simon Garnier <simongarnierg@gmail.com>
2015-11-30 21:27:37 -0500

    model admin now match db representation

--- app/admin/availability.rb
+++ app/admin/availability.rb
@@ -1,13 +1,12 @@
 ActiveAdmin.register Availability, namespace: :super_admin do
   menu parent: "Entities"
-  permit_params :availability_per_week_id, :availability_duration_id
+  permit_params :per_week, :duration
 
   index do
     selectable_column
     column :id
-    column "Name" do |availability|
-      link_to availability.name, super_admin_availability_path(availability)
-    end
+    column :per_week
+    column :duration
     actions
   end
 end
