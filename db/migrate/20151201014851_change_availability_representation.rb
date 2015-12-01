db91a3ddeff35e953659f26eecb571557d83fea6
Simon Garnier <simongarnierg@gmail.com>
2015-11-30 21:24:14 -0500

    changed db representation

---
+++ db/migrate/20151201014851_change_availability_representation.rb
@@ -0,0 +1,26 @@
+class ChangeAvailabilityRepresentation < ActiveRecord::Migration
+  def change
+    add_column :availabilities, :per_week, :string, null:true
+    add_column :availabilities, :duration, :string, null:true
+
+    Availability.all.each do |availability|
+      availability.duration = availability.availability_duration.name
+      availability.per_week = availability.availability_per_week.name
+      availability.save
+    end
+
+    change_column_null :availabilities, :per_week, false
+    change_column_null :availabilities, :duration, false
+
+    remove_column :availabilities, :availability_per_week_id
+    remove_column :availabilities, :availability_duration_id
+
+    drop_table :availability_durations
+    drop_table :availability_per_weeks
+  end
+
+  def down
+    remove_column :availabilities, :per_week
+    remove_column :availabilities, :duration
+  end
+end
