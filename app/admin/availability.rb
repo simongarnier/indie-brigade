861d055f4de59ebf07f7c8b3aac78ee053ca8d2b
Simon Garnier <simongarnierg@gmail.com>
2016-01-10 16:50:27 -0500

    availability admin needs some work

--- app/admin/availability.rb
+++ app/admin/availability.rb
@@ -1,12 +1,13 @@
 ActiveAdmin.register Availability, namespace: :super_admin do
   menu parent: "Entities"
-  permit_params :per_week, :duration
+  permit_params :availability_per_week_id, :availability_duration_id
 
   index do
     selectable_column
     column :id
-    column :per_week
-    column :duration
+    column "Name" do |availability|
+      link_to availability.name, super_admin_availability_path(availability)
+    end
     actions
   end
 end
