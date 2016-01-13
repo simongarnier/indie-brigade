bd9f14661d588f96671e8157a6012ef04673ccd6
Simon Garnier <simongarnierg@gmail.com>
2016-01-12 23:16:40 -0500

    profile image can now be changed for a dev using the edit route

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -1,4 +1,15 @@
 class DevsController < ApplicationController
+  def edit
+    @dev = Dev.find(params[:id])
+  end
+
+  def update
+    @dev = Dev.find(params[:id])
+    @dev.avatar = dev_params[:avatar]
+    @dev.save!
+    redirect_to edit_dev_path
+  end
+
   # GET /devs
   def index
     @devs = Dev.all
@@ -12,6 +23,6 @@ class DevsController < ApplicationController
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def dev_params
-      params.require(:dev).permit(:name, :description, :role_id, :availability_id)
+      params.require(:dev).permit(:name, :description, :role_id, :avatar)
     end
 end
