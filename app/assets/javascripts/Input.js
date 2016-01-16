5e8f390cc0100d221731f803367320c801428a84
Sonda <sonda@Sondas-MacBook-Pro.local>
2016-01-16 14:12:22 -0500

    fix input

--- app/assets/javascripts/Input.js
+++ app/assets/javascripts/Input.js
@@ -5,7 +5,7 @@
     var input = $(this).find("input[type='text']");
     var label = $(this).find("label");
     input.focus(function(){
-      label.animate({'font-size' : '0.75rem', 'top' : '-15px'}, 500);
+      label.animate({'font-size' : '0.75rem', 'top' : '-15px'}, 150);
     })
     input.on('blur',function(){
       if($(this).val() == ""){
