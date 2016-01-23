1baec01ef05a5e22a54d5ae4d6a9770de7069af3
Simon Garnier <simongarnierg@gmail.com>
2016-01-22 20:28:40 -0500

    tabs to spaces

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-	$(".favorite-link").on("click", function(e){
-		var star = $(e.target);
-		if(star.hasClass("fa-star-o")){
-			star.addClass("fa-star");
-			star.removeClass("fa-star-o");
-		} else {
-			star.addClass("fa-star-o");
-			star.removeClass("fa-star");
-		}
-	});
+  $(".chosen-select").chosen()
+  $(".favorite-link").on("click", function(e){
+    var star = $(e.target);
+    if(star.hasClass("fa-star-o")){
+      star.addClass("fa-star");
+      star.removeClass("fa-star-o");
+    } else {
+      star.addClass("fa-star-o");
+      star.removeClass("fa-star");
+    }
+  });
 });
