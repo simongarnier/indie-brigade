afcf027555c4ddf211e289951fcff0ced1a5c73e
Simon Garnier <simongarnierg@gmail.com>
2016-02-15 21:16:32 -0500

    icon helper

--- app/models/software.rb
+++ app/models/software.rb
@@ -3,4 +3,8 @@ class Software < ActiveRecord::Base
 
   has_many :dev_conditions, dependent: :delete_all
   has_many :devs, through: :dev_conditions
+
+  def icon_path
+    "/images/softwareicons/#{filename}"
+  end
 end
