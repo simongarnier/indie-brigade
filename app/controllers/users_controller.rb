80333c7d97f8e36a401c6f1623c0f6b427c5a74e
Simon Garnier <simongarnierg@gmail.com>
2016-03-25 18:25:10 -0400

    selecting a main skill when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < Clearance::UsersController
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = user_from_params
 
-    if @user.password == @cpassword && @confirm && verify_recaptcha(model: @user) && @user.save 
+    if @user.password == @cpassword && @confirm && verify_recaptcha(model: @user) && @user.save
       sign_in @user
+      dev = Dev.new
+      dev.user = @user
+      skill = Skill.find(@main_skill_id)
+      if skill
+          dev.main_skill = skill
+          dev.role = skill.role
+      end
+      dev.save
       redirect_back_or url_after_create
     else
       render template: "users/new"
@@ -18,6 +30,8 @@ class UsersController < Clearance::UsersController
     password = user_params.delete(:password)
     @cpassword = user_params.delete(:cpassword)
     @confirm = user_params.delete(:confirm)
+    dev = user_params.delete(:dev)
+    @main_skill_id = dev[:main_skill_id]
 
     Clearance.configuration.user_model.new(user_params).tap do |user|
       user.firstname = firstname
